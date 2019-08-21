a6ba5a6dcde60d8900ce9fda1989e03767c98ddc
Julio Muller <juliodolago@gmail.com>
2019-08-21 20:20:04 -0300

    Rearranged HTML tags/attributes for better visualization

--- src/views/app/MyProfile.vue
+++ src/views/app/MyProfile.vue
@@ -1,34 +1,69 @@
 <template>
   <div>
     <h1 :class="{ 'mb-4': isEditing }">Meus empréstimos</h1>
-    <button type="button" class="btn btn-sm btn-hero mb-4" v-show="!isEditing" @click="edit">
+    <button
+      type="button"
+      class="btn btn-sm btn-hero mb-4"
+      v-show="!isEditing"
+      @click="edit"
+    >
       Editar Dados
     </button>
     <form>
       <div class="form-group">
         <label for="name">Nome Completo:</label>
-        <input type="text" id="name" :class="getClasses" :value="user.name" required>
+        <input
+          type="text"
+          id="name"
+          v-model="user.name"
+          :class="getClasses"
+        />
       </div>
       <div class="form-group">
         <label for="email">Email de Contato:</label>
-        <input type="email" id="email" :class="getClasses" :value="user.email" required>
+        <input
+          type="email"
+          id="email"
+          v-model="user.email"
+          :class="getClasses"
+        />
       </div>
       <div class="card border-hero p-3 bg-white w-50" v-show="isEditing">
         <h4 class="mb-4">Mudar Senha</h4>
         <div class="form-group">
           <label for="oldPassword">Senha atual:</label>
-          <input type="password" id="oldPassword" :class="getClasses" placeholder="Deve ter no mínimo 8 digitos">
+          <input
+            type="password"
+            id="oldPassword"
+            placeholder="Deve ter no mínimo 8 digitos"
+            :class="getClasses"
+          />
         </div>
         <div class="form-group">
           <label for="newPassword">Nova Senha:</label>
-          <input type="password" id="newPassword" :class="getClasses" placeholder="Deve ter no mínimo 8 digitos">
+          <input
+            type="password"
+            id="newPassword"
+            placeholder="Deve ter no mínimo 8 digitos"
+            :class="getClasses"
+          />
         </div>
         <div class="form-group">
           <label for="newPasswordConfirmation">Repetir a Nova Senha:</label>
-          <input type="password" id="newPasswordConfirmation" :class="getClasses" placeholder="Deve ser igual a nova senha">
+          <input
+            type="password"
+            id="newPasswordConfirmation"
+            placeholder="Deve ser igual a nova senha"
+            :class="getClasses"
+          />
         </div>
       </div>
-      <button type="submit" class="btn btn-hero m-3" v-show="isEditing" @click="save($event)">
+      <button
+        type="submit"
+        class="btn btn-hero m-3"
+        v-show="isEditing"
+        @click="save($event)"
+      >
         Salvar
       </button>
     </form>
