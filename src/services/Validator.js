2e09d1f3112c4fb9acb679e67d1ea9ae60c176eb
Julio Muller <juliodolago@gmail.com>
2019-08-21 22:57:32 -0300

    Fixed validation methods

--- src/services/Validator.js
+++ src/services/Validator.js
@@ -10,15 +10,26 @@ export default (function() {
     .is().min(8)
     .is().max(32)
     .has().letters()
-    .has().lowercase()
     .has().digits()
     .has().not().spaces()
 
   // Associate functionalitis to methods
   return {
-    matchesPassword = password.validate,
-    matchesEmail = EmailValidator.validate,
-    matchesName = string => string.length <= 255
+    passwordHasErrors: string => {
+      return password.validate(string)
+        ? null
+        : { password: 'A senha deve ter entre 8 e 32 caracteres, possuir pelo menos um número e uma letra, sem espaços' }
+    },
+    emailHasErrors: string => {
+      return EmailValidator.validate(string)
+        ? null
+        : { email: 'Não corresponde a um email válido' }
+    },
+    nameHasErrors: string => {
+      return string.length >= 3 && string.length <= 90
+        ? null
+        : { name: 'O nome deve ter entre 3 e 90 caracteres' }
+    }
   }
 
 })()
