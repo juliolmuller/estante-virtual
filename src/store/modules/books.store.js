b86408b1c92245cb21890e3002526b93513d241c
Julio Muller <juliodolago@gmail.com>
2020-12-31 14:48:12 -0300

    Updated books store module

--- src/store/modules/books.store.js
+++ src/store/modules/books.store.js
@@ -1,57 +1,47 @@
-/* eslint-disable no-return-assign */
 import { booksApi } from '../../services/api'
 
 export default {
+  namespaced: 'books',
 
   state: {
+    isFetching: false,
     books: [],
-    userBooks: [],
-    userLoans: [],
-    searchCriteria: '',
-    filterCriteria: {
-      available: true,
-      unavailable: false,
-    },
   },
 
   getters: {
+    isFetching: (state) => state.isFetching,
     allBooks: (state) => state.books,
-    userBooks: (state) => state.userBooks,
-    userLoans: (state) => state.userLoans,
-    filterCriteria: (state) => state.filterCriteria,
-    searchCriteria: (state) => state.searchCriteria,
-    results: (state) => {
-      return state.books.filter((book) => {
-        return (state.filterCriteria.available && !book.loan) || (state.filterCriteria.unavailable && book.loan)
-      }).filter((book) => {
-        return book.name.toLowerCase().indexOf(state.searchCriteria.toLowerCase()) > -1
-      })
+    availableBooks: (state) => state.books.filter((book) => !book.loan),
+    borrowedBooks: (state) => state.books.filter((book) => book.loan),
+    userBooks(state, _getters, _rootState, rootGetters) {
+      const userId = rootGetters['auth/userData'].id
+
+      return state.books.filter((book) => book.userId === userId)
+    },
+    userLoans(state, _getters, _rootState, rootGetters) {
+      const userId = rootGetters['auth/userData'].id
+
+      return state.books.filter((book) => book.loan?.userId === userId)
     },
   },
 
   mutations: {
-    setBooks: (state, books) => state.books = books,
-    setUserBooks: (state, books) => state.userBooks = books,
-    setUserLoans: (state, books) => state.userLoans = books,
-    setFilter: (state, filter) => state.filterCriteria = filter,
-    setSearch: (state, search) => state.searchCriteria = search,
+    setFetching(state, fetchingStatus) {
+      state.isFetching = fetchingStatus
+    },
+    toggleFetching(state) {
+      state.isFetching = !state.isFetching
+    },
+    setBooks(state, books) {
+      state.books = books
+    },
   },
 
   actions: {
-    async fetchBooks({ commit }) {
+    async fetchAll({ commit }) {
+      commit('setFetching', true)
       commit('setBooks', await booksApi.get())
-    },
-    async fetchUserBooks({ commit }, userId) {
-      commit('setUserBooks', await booksApi.get({ userId }))
-    },
-    async fetchUserLoans({ commit }, userId) {
-      commit('setUserLoans', await booksApi.get({ 'loan.userId': userId }))
-    },
-    setFilter({ commit }, { available, unavailable }) {
-      commit('setFilter', { available, unavailable })
-    },
-    setSearch({ commit }, searchTerm) {
-      commit('setSearch', searchTerm)
+      commit('setFetching', false)
     },
   },
 }
