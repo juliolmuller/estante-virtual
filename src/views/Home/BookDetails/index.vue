8519ad068cfbeb3b8bf1388738beba01d6961626
Julio Muller <juliodolago@gmail.com>
2021-01-05 01:02:37 -0300

    Finished component to display book details

--- src/views/Home/BookDetails/index.vue
+++ src/views/Home/BookDetails/index.vue
@@ -1,224 +1,313 @@
 <script>
 import { mapGetters, mapActions } from 'vuex'
-import { booksApi, usersApi } from '@/services/api'
+import truncate from '@/filters/truncate'
+import ViewTitle from '@/components/ViewTitle'
 
 export default {
+  name: 'BookDetails',
+
+  filters: {
+    truncate,
+  },
+
+  components: {
+    ViewTitle,
+  },
 
   props: {
-    bookId: String,
+    bookId: {
+      type: [Number, String],
+      required: true,
+    },
   },
 
   data: () => ({
-    formTitle: '',
-    book: {},
-    ownerName: '',
-    userName: '',
+    isLoading: false,
     isEditing: false,
+    book: {},
   }),
 
   computed: {
     ...mapGetters({
-      thisUserId: 'userId',
-      thisUserName: 'userName',
+      appLoading: 'isLoading',
+      books: 'books/allBooks',
+      users: 'users/allUsers',
+      user: 'auth/userData',
     }),
-    getClasses() {
+    owner() {
+      const owner = this.users.find((user) => user.id === this.book.userId) || {}
+      const belongsToUser = this.book.userId === this.user.id
+
+      return {
+        ...owner,
+        name: belongsToUser ? 'você' : owner.name,
+      }
+    },
+    inputStyle() {
       return {
         'form-control': this.isEditing,
-        'border-hero': this.isEditing,
         'form-control-plaintext': !this.isEditing,
-        'h4': !this.isEditing,
-        'pt-0': !this.isEditing,
+        'font-weight-bold': !this.isEditing,
+        'text-hero': !this.isEditing,
       }
     },
   },
 
   methods: {
-    ...mapActions(['fetchBooks']),
-    goBack() {
-      this.$router.go(-1)
-    },
-    async loanBook() {
-      this.book = await booksApi.put({
-        ...this.book,
-        loan: {
-          userId: this.thisUserId,
-          date: (new Date()).toISOString().substring(0, 10),
-        },
-      })
-      this.userName = 'você'
-    },
-    async returnBook() {
-      this.book.loan = false
-      this.bbok = await booksApi.put(this.book)
-    },
-    edit() {
-      this.isEditing = !this.isEditing
-      this.ownerName = this.thisUserName
-    },
-    async save(event) {
-      event.preventDefault()
-      event.target.disabled = true
+    ...mapActions('books', {
+      createBook: 'create',
+      updateBook: 'update',
+      borrowBook: 'borrow',
+      returnBook: 'return',
+      deleteBook: 'delete',
+    }),
+    async handleSubmit() {
+      this.isLoading = true
 
-      if (this.bookId === 'novo') {
-        this.book.loan = false
-        this.book = await booksApi.post(this.book)
-        event.target.disabled = false
-        this.isEditing = false
-        this.$router.push({
-          name: 'bookDetails',
-          params: { bookId: JSON.stringify(this.book.id) },
-        })
-      } else {
-        this.book = await booksApi.put(this.book)
-        event.target.disabled = false
-        this.isEditing = false
+      try {
+        if (this.bookId === 'novo') {
+          const book = await this.createBook(this.book)
+          const route = { name: 'BookDetails', params: { bookId: book.id } }
+
+          this.$router.replace(route)
+        } else {
+          await this.updateBook(this.book)
+          this.isEditing = false
+        }
+      } catch (error) {
+        throw error
+      } finally {
+        this.isLoading = false
       }
     },
-    async drop(event) {
-      if (confirm(`Tem certeza de que deseja remover o livro "${this.book.name}" do catálogo?`)) {
-        await booksApi.delete(this.book.id)
+    async handleBorrow() {
+      this.isLoading = true
 
-        event.target.disabled = false
-        this.isEditing = false
-        this.$router.push({
-          name: 'bookDetails',
-          params: { bookId: JSON.stringify(this.book.id) },
-        })
+      try {
+        await this.borrowBook(this.book)
+      } catch (error) {
+        throw error
+      } finally {
+        this.isLoading = false
+      }
+    },
+    async handleReturn() {
+      this.isLoading = true
+
+      try {
+        await this.returnBook(this.book)
+      } catch (error) {
+        throw error
+      } finally {
+        this.isLoading = false
+      }
+    },
+    async handleDelete() {
+      if (confirm(`Tem certeza de que deseja remover o livro "${this.book.name}" do catálogo?`)) {
+        this.isLoading = true
+
+        try {
+          await this.deleteBook(this.book)
+          this.$router.replace({ name: 'Home' })
+        } catch (error) {
+          throw error
+        } finally {
+          this.isLoading = true
+        }
       }
     },
   },
 
-  async created() {
-    if (this.bookId === 'novo') {
-      this.isEditing = true
-      this.formTitle = 'Novo Livro'
-      this.ownerName = this.thisUserName
-      this.book.userId = this.thisUserId
-    } else {
-      this.book = await booksApi.getOne(this.bookId)
-      this.formTitle = this.book.name
-      this.ownerName = (await usersApi.getOne(this.book.userId)).name
+  async mounted() {
+    await new Promise((resolve) => {
+      while (this.appLoading) {} // eslint-disable-line no-empty
+      resolve()
+    })
 
-      if (this.book.loan) {
-        if (this.book.loan.userId === this.thisUserId) {
-          this.userName = 'você'
-        } else {
-          this.userName = (await usersApi.getOne(this.book.loan.userId)).name
-        }
-      }
+    if (this.bookId === 'novo') {
+      this.book.userId = this.user.id
+      this.book.loan = false
+      this.isEditing = true
+      return
+    }
+
+    this.book = this.books.find((book) => book.id === this.bookId)
+
+    if (!this.book) {
+      this.$router.replace({ name: 'Home' })
     }
   },
 }
 </script>
 
 <template>
-  <div>
-    <button
-      type="button"
-      class="btn btn-sm btn-light float-right"
-      v-if="!isEditing"
-      @click="goBack"
-    >Voltar</button>
+  <div id="book-details">
+    <header>
+      <ViewTitle>
+        {{ bookId === 'novo' ? 'Novo Livro' : book.name | truncate(80) }}
+      </ViewTitle>
 
-    <h1 class="mb-4 text-truncate">{{ formTitle }}</h1>
-    <form @submit="save">
+      <button
+        type="button"
+        class="btn btn-sm btn-light"
+        v-if="!isEditing"
+        @click="$router.go(-1)"
+      >
+        <img src="@/assets/left-arrow.svg" alt="ícone de retornar" />
+        Voltar
+      </button>
+    </header>
+
+    <form @submit.prevent="handleSubmit">
       <div class="row">
-        <div class="col-12 col-md-4" :style="{ visibility: book.image ? 'visible' : 'hidden' }">
-          <img class="img-fluid" alt="Capa do livro" :src="book.image">
+        <div class="col-12 col-md-4" v-if="book.image">
+          <img class="img-fluid" alt="Capa do livro" :src="book.image" />
         </div>
         <div class="col-12 col-md-8">
-          <div class="row">
-            <div class="col-12">
-              <div class="form-group" v-if="isEditing && bookId != 'novo'">
-                <label for="id">ID:</label>
-                <input
-                  type="text"
-                  id="id"
-                  disabled
-                  v-model="book.id"
-                  :class="{ ...getClasses, 'text-hero': !this.isEditing }"
-                />
-              </div>
-              <div class="form-group">
-                <label for="name">Título:</label>
-                <input
-                  type="text"
-                  id="name"
-                  required autofocus
-                  v-model="book.name"
-                  :class="{ ...getClasses, 'text-hero': !this.isEditing }"
-                />
-              </div>
-              <div class="form-group" v-if="isEditing">
-                <label for="image">URL da Capa:</label>
-                <input
-                  type="text"
-                  id="image"
-                  required
-                  v-model="book.image"
-                  :class="{ ...getClasses, 'text-hero': !this.isEditing }"
-                />
-              </div>
-              <input type="hidden" name="userId" v-model="book.userId" />
-              <div class="form-group">
-                <label for="ownerName">Dono do Livro:</label>
-                <input
-                  type="text"
-                  id="ownerName"
-                  :disabled="isEditing"
-                  :value="ownerName"
-                  :class="{ ...getClasses, 'text-hero': !this.isEditing }"
-                />
-              </div>
-              <div class="form-group" v-if="!isEditing">
-                <label for="name">Status:</label>
-                <input
-                  type="text"
-                  id="name"
-                  :class="getClasses"
-                  :style="{ 'color': !!book.loan ? 'red' : 'green' }"
-                  :value="book.loan
-                    ? `Emprestado a ${userName} desde ${(new Date('2019-03-30')).toLocaleDateString()}`
-                    : 'Disponível'
-                  "
-                />
-              </div>
-            </div>
-            <div class="rcol-12">
-              <button
-                type="button"
-                class="btn btn-hero m-3"
-                v-if="!isEditing && !book.loan"
-                @click="loanBook"
-              >Emprestar Livro</button>
-              <button
-                type="button"
-                class="btn btn-hero m-3"
-                v-if="!isEditing && book.loan && book.userId == thisUserId"
-                @click="returnBook"
-              >Devolver Livro</button>
-              <button
-                type="button"
-                class="btn btn-info m-3"
-                v-if="!isEditing && book.userId == thisUserId"
-                @click="edit"
-              >Editar Dados</button>
-              <button
-                type="submit"
-                class="btn btn-primary m-3"
-                v-if="isEditing"
-              >Salvar</button>
-              <button
-                type="button"
-                class="btn btn-danger m-3"
-                v-if="isEditing && bookId != 'novo'"
-                :disabled="book.loan"
-                @click="drop"
-              >Remover da Estante</button>
-            </div>
+          <div class="form-group" v-if="bookId !== 'novo'">
+            <label for="book-id">Código de Cadastro:</label>
+            <input
+              type="text"
+              id="book-id"
+              :class="inputStyle"
+              v-model.number="book.id"
+              readonly
+            />
+          </div>
+          <div class="form-group">
+            <label for="book-name">Título da Obra:</label>
+            <input
+              type="text"
+              id="book-name"
+              :class="inputStyle"
+              v-model.trim="book.name"
+              :readonly="!isEditing"
+              required
+              autofocus
+            />
+          </div>
+          <div class="form-group" v-if="isEditing">
+            <label for="book-image">URL da Capa:</label>
+            <input
+              type="text"
+              id="book-image"
+              :class="inputStyle"
+              v-model.trim="book.image"
+              :readonly="!isEditing"
+              required
+            />
+          </div>
+          <div class="form-group" v-if="!isEditing">
+            <label for="book-owner">Dono do Livro:</label>
+            <input
+              type="text"
+              id="book-owner"
+              :class="inputStyle"
+              :value="owner.name"
+              readonly
+            />
+          </div>
+          <div class="form-group" v-if="!isEditing">
+            <label for="status">Status:</label>
+            <input
+              type="text"
+              id="status"
+              :class="{
+                ...inputStyle,
+                'text-hero': false,
+                'text-success': !book.loan,
+                'text-danger': !!book.loan
+              }"
+              :value="book.loan
+                ? `Emprestado desde ${(new Date(book.loan.date)).toLocaleDateString()}`
+                : 'Disponível'"
+              readonly
+            />
+          </div>
+
+          <div class="action-btn">
+            <button
+              type="button"
+              class="btn btn-hero"
+              :disabled="isLoading"
+              v-if="!isEditing && !book.loan"
+              @click="handleBorrow"
+            >Emprestar Livro</button>
+            <button
+              type="button"
+              class="btn btn-hero"
+              :disabled="isLoading"
+              v-if="!isEditing && book.loan && book.loan.userId === user.id"
+              @click="handleReturn"
+            >Devolver Livro</button>
+            <button
+              type="button"
+              class="btn btn-danger mr-auto"
+              v-if="isEditing && !book.loan && owner.id === user.id && bookId != 'novo'"
+              :disabled="isLoading"
+              @click="handleDelete"
+            >Remover da Estante</button>
+            <button
+              type="button"
+              :class="['btn', isEditing ? 'btn-light' : 'btn-secondary']"
+              v-if="!book.loan && owner.id === user.id"
+              @click="isEditing = !isEditing"
+            >{{ isEditing ? 'Cancelar' : 'Editar Dados' }}</button>
+            <button
+              type="submit"
+              class="btn btn-hero"
+              :disabled="isLoading"
+              v-if="isEditing"
+            >Concluído</button>
           </div>
         </div>
       </div>
     </form>
   </div>
 </template>
+
+<style lang="scss">
+#book-details {
+  & > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    & > [type="button"] {
+      margin-bottom: 3rem;
+
+      & > img {
+        height: 8px;
+      }
+    }
+  }
+
+  & .form-group {
+    margin-bottom: 1rem;
+
+    & label {
+      margin-bottom: 0;
+    }
+  }
+
+  & .form-control {
+    margin: 0.5rem 0 1.5rem;
+    outline: none;
+  }
+
+  & .form-control-plaintext {
+    outline: none;
+    font-size: large;
+  }
+
+  & .action-btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    & .btn {
+      margin: auto 0.5rem;
+    }
+  }
+}
+</style>
